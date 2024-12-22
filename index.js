cad53368a6a34a1703cede1bf61689313d6db3a5
jyotiprokash <jyotiprokash2000@gmail.com>
2024-12-23 03:41:00 +0600

    connect  mongodb and post cars data/get data

--- index.js
+++ index.js
@@ -3,11 +3,65 @@ const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middleware
 
 app.use(cors());
 app.use(express.json());
+
+// mongodb
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ti2zy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const carRentalCollection = client.db("carREntalDB").collection("cars");
+    // all AvailableCars
+    app.get("/cars", async (req, res) => {
+      const cursor = carRentalCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/cars", async (req, res) => {
+      const newCar = req.body;
+      const result = await carRentalCollection.insertOne(newCar);
+      res.send(result);
+    });
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
 
 
 app.get("/", (req, res) => {
